0e3a2624be6e3df5e0a615bb38044e3158fb90d5
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2023-12-22 17:27:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ lessons = config.get('User', 'LESSONS')
 
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': f"Bearer {os.getenv('JWT_TOKEN')}",
+    'Authorization': 'Bearer ' + token,
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
 }
 
