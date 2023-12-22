f10ec71317a1904163e4c70a5bacea58404082a1
GorouFlex <98001973+gorouflex@users.noreply.github.com>
2023-12-22 18:23:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -204,9 +204,10 @@ for i in range(int(lessons)):
             'shouldLearnThings': True,
         },
     )
-    response = requests.put(f'https://www.duolingo.com/2017-06-30/sessions/{session["id"]}', data=json.dumps(end_response), headers=headers)
+    
+    end_data = end_response.json()
+    response = requests.put(f'https://www.duolingo.com/2017-06-30/sessions/{session["id"]}', data=json.dumps(end_data), headers=headers)
     if response.status_code != 200:
         print(f"{colors.FAIL}Error: {response.status_code}, {response.text}{colors.ENDC}")
         continue
-    end_data = end_response.json()
     print(f"{colors.OKGREEN}[{i+1}] - Gained: {end_data['xpGain']} XP (✓){colors.ENDC}")
